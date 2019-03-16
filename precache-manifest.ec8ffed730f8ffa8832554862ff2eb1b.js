05b830a6a2f22bf648e5f28af4ff497fb7a861fd
maciekwilliam <maciejwillem@gmail.com>
2019-03-16 12:16:31 +0100

    Updates

---
+++ precache-manifest.ec8ffed730f8ffa8832554862ff2eb1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8eed06dde3666c48872",
+    "url": "/todoapp/static/js/runtime~main.9160f320.js"
+  },
+  {
+    "revision": "14678efe807ba83a1de8",
+    "url": "/todoapp/static/js/main.fafbbc67.chunk.js"
+  },
+  {
+    "revision": "9c306fa42396360d8a99",
+    "url": "/todoapp/static/js/2.f73b82d1.chunk.js"
+  },
+  {
+    "revision": "14678efe807ba83a1de8",
+    "url": "/todoapp/static/css/main.f2a48ac0.chunk.css"
+  },
+  {
+    "revision": "d64ac2432e3a058fcf632321be2ff100",
+    "url": "/todoapp/index.html"
+  }
+];
